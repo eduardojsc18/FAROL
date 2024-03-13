79677ce83a78450bcf25920c4e14ddc793e2c1b9
Eduardo José <eduardojsc18@outlook.com>
2024-03-13 20:46:30 -0300

    adiciona logica para pegar o novo access_token apenas quando ele expirar ou não existir no banco

--- server/api/access-token.get.js
+++ server/api/access-token.get.js
@@ -1,17 +1,21 @@
 export default defineEventHandler(async (event) => {
 
+    const storage = useStorage('data')
+    let tokens = await storage.getItem('tokens')
+
+    // Verifica se o token já expirou, caso não, envia o access_token salvo no banco
+    if ((new Date() - new Date(tokens?.updated_at)) / 1000 < tokens?.expires_in) {
+        return tokens.access_token
+    }
+
     const config = useRuntimeConfig(event)
     const baseURL = config.public.apiBase
 
-    const storage = useStorage('data')
-    let tokens = await storage.getItem('tokens')
     let versionTokens = {
         old_token: tokens?.new_token ?? config.apiToken,
         new_token: '',
-        expires_in: tokens?.expires_in ?? ''
+        expires_in: tokens?.expires_in ?? '',
     }
-
-    // TODO: Fazer logica para pegar um novo token apenas após o vencimento da expiração
 
     const token = await $fetch(`/oauth/token`, {
         method: 'POST',
@@ -30,6 +34,8 @@ export default defineEventHandler(async (event) => {
 
     versionTokens.new_token = token.refresh_token
     versionTokens.expires_in = token.expires_in
+    versionTokens.access_token = token.access_token
+    versionTokens.updated_at = new Date()
 
     await storage.setItem('tokens', versionTokens)
 
